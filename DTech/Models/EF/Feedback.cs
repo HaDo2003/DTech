f78c3845a8d438bb5d1bef5baeb2b8cdfb6f4b02
HaDo2003 <118713300+HaDo2003@users.noreply.github.com>
2024-08-29 00:10:00 +0700

    Add Feedback

--- DTech/Models/EF/Feedback.cs
+++ DTech/Models/EF/Feedback.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DTech.Models.EF;
 
 public partial class Feedback
 {
+    [Key]
     public int FeedbackId { get; set; }
 
+    [Required(ErrorMessage = "Please enter name to give feedback")]
     public string? Name { get; set; }
 
+    [Required(ErrorMessage = "Please enter email to give feedback")]
+    [EmailAddress(ErrorMessage = "Imvalid email address")]
     public string? Email { get; set; }
 
+    [Required(ErrorMessage = "Please write feedback")]
     public string? Detail { get; set; }
 
+    [Display(Name = "Feedback Date")]
     public DateTime? Fbdate { get; set; }
 }
