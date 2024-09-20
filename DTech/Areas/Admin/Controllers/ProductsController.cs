f6856ce191ec671f869f900df938fec2d2f9dc58
Ha Do <118713300+HaDo2003@users.noreply.github.com>
2024-09-20 17:21:09 +0700

    Update edit specification

--- DTech/Areas/Admin/Controllers/ProductsController.cs
+++ DTech/Areas/Admin/Controllers/ProductsController.cs
@@ -143,7 +143,9 @@ namespace DTech.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            var product = await _context.Products.FindAsync(id);
+            var product = await _context.Products
+                .Include(p => p.Specifications)
+                .FirstOrDefaultAsync(p => p.ProductId == id);
             if (product == null)
             {
                 return NotFound();
@@ -314,6 +316,74 @@ namespace DTech.Areas.Admin.Controllers
 
             TempData["message"] = JsonConvert.SerializeObject(new XMessage("success", "Edited successfully"));
             return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult SaveSpecifications(int productId, List<Specification> specifications)
+        {
+            if (ModelState.IsValid)
+            {
+                var product = _context.Products.Include(p => p.Specifications)
+                                               .FirstOrDefault(p => p.ProductId == productId);
+                if (product != null)
+                {
+                    // Loop through the submitted specifications
+                    foreach (var spec in specifications)
+                    {
+                        // Generate slug
+                        var slug = spec.SpecName.ToLower().Replace(" ", "-");
+
+                        var existingSpec = product.Specifications
+                                                  .FirstOrDefault(s => s.SpecId == spec.SpecId);
+
+                        if (existingSpec != null)
+                        {
+                            // Update existing specification
+                            existingSpec.SpecName = spec.SpecName;
+                            existingSpec.Detail = spec.Detail;
+                            // Update slug only if the name has changed
+                            if (existingSpec.Slug != slug)
+                            {
+                                existingSpec.Slug = slug;
+                            }
+                        }
+                        else
+                        {
+                            // Add new specification
+                            product.Specifications.Add(new Specification
+                            {
+                                SpecName = spec.SpecName,
+                                Detail = spec.Detail,
+                                Slug = slug,
+                                ProductId = productId
+                            });
+                        }
+                    }
+
+                    _context.SaveChanges();
+                    TempData["message"] = JsonConvert.SerializeObject(new XMessage("success", "Edited successfully"));
+                    return RedirectToAction("Edit", new { id = productId });
+                }
+            }
+            TempData["message"] = JsonConvert.SerializeObject(new XMessage("success", "Edited failed"));
+            return View(specifications);  // Return to the same view if invalid
+        }
+
+        [HttpPost]
+        [Route("RemoveSpecification")]
+        public IActionResult RemoveSpecification(int specificationId)
+        {
+            var specification = _context.Specifications.Find(specificationId);
+            if (specification != null)
+            {
+                _context.Specifications.Remove(specification);
+                _context.SaveChanges();
+                return Json(new { success = true, message = "Specification deleted successfully." });
+            }
+            else
+            {
+                return Json(new { success = false, message = "Specification not found." });
+            }
         }
     }
 }
---
+++ DTech/Areas/Admin/Controllers/SpecificationsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DTech.Models.EF;
+
+namespace DTech.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SpecificationsController : Controller
+    {
+        private readonly EcommerceWebContext _context;
+
+        public SpecificationsController(EcommerceWebContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Specifications
+        public async Task<IActionResult> Index()
+        {
+            var ecommerceWebContext = _context.Specifications.Include(s => s.Product);
+            return View(await ecommerceWebContext.ToListAsync());
+        }
+
+        // GET: Admin/Specifications/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var specification = await _context.Specifications
+                .Include(s => s.Product)
+                .FirstOrDefaultAsync(m => m.SpecId == id);
+            if (specification == null)
+            {
+                return NotFound();
+            }
+
+            return View(specification);
+        }
+
+        // GET: Admin/Specifications/Create
+        public IActionResult Create()
+        {
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "Name");
+            return View();
+        }
+
+        // POST: Admin/Specifications/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SpecId,ProductId,SpecName,Slug,Detail")] Specification specification)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(specification);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "Name", specification.ProductId);
+            return View(specification);
+        }
+
+        // GET: Admin/Specifications/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var specification = await _context.Specifications.FindAsync(id);
+            if (specification == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "Name", specification.ProductId);
+            return View(specification);
+        }
+
+        // POST: Admin/Specifications/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SpecId,ProductId,SpecName,Slug,Detail")] Specification specification)
+        {
+            if (id != specification.SpecId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(specification);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SpecificationExists(specification.SpecId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "Name", specification.ProductId);
+            return View(specification);
+        }
+
+        // GET: Admin/Specifications/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var specification = await _context.Specifications
+                .Include(s => s.Product)
+                .FirstOrDefaultAsync(m => m.SpecId == id);
+            if (specification == null)
+            {
+                return NotFound();
+            }
+
+            return View(specification);
+        }
+
+        // POST: Admin/Specifications/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var specification = await _context.Specifications.FindAsync(id);
+            if (specification != null)
+            {
+                _context.Specifications.Remove(specification);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SpecificationExists(int id)
+        {
+            return _context.Specifications.Any(e => e.SpecId == id);
+        }
+    }
+}
